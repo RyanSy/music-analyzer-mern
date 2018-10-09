89b9e9c9f0355abede92e69a3475a656ee838d7e
Ryan Sy <ryanbsy@gmail.com>
2018-10-09 10:07:10 -0400

    deployed to redhat openshift

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
-#!/usr/bin/env node
+var express = require('express');
+var favicon = require('serve-favicon');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 /**
  * Module dependencies.
  */
-
-var app = require('../app');
 var debug = require('debug')('music-analyzer-mern:server');
 var http = require('http');
 
@@ -12,8 +29,15 @@ var http = require('http');
  * Get port from environment and store in Express.
  */
 
-var port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
+// var port = normalizePort(process.env.PORT || '3000');
+// app.set('port', port);
+
+/*
+Check for redhat. Original settings commented out.
+*/
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 /**
  * Create HTTP server.
@@ -25,7 +49,11 @@ var server = http.createServer(app);
  * Listen on provided port, on all network interfaces.
  */
 
-server.listen(port);
+server.listen(server_port, server_ip_address, function () {
+   console.log( "Listening on " + server_ip_address + ", port " + server_port +"\n" )
+ });
+
+// server.listen(port);
 server.on('error', onError);
 server.on('listening', onListening);
 
